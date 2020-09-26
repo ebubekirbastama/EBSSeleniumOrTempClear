44c46edf54199b7ebe6e74b6c4092261a309fc77
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-09-27 02:40:51 +0300

    Selenium or Temp all Folder Clear

---
+++ EBSSeleniumTempClear/EBSSeleniumTempClear.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace EBSSeleniumTempClear
+{
+    public class EBSSeleniumTempClear
+    {
+        string dizin = @"C:\Users\" + Environment.UserName + @"\AppData\Local\Temp";
+        int deleteid = 1;
+        public void seleniumCookiedelete(Label lbl,RichTextBox rc)
+        {
+            string[] FolderListing = Directory.GetDirectories(dizin);
+            for (int i = 0; i < FolderListing.Length; i++)
+            {
+                if (FolderListing[i].IndexOf("scoped") != -1)
+                {
+                    rc.AppendText(FolderListing[i].ToString() + "\r");
+                    lbl.Text = "Delete Cookie :" + deleteid;deleteid++;
+                    try
+                    {
+                        Directory.Delete(FolderListing[i], true);
+                    }
+                    catch
+                    {
+                        Directory.Delete(FolderListing[i], true);
+                    }
+                }
+            }
+        }
+        public void TempClear(Label lbl, RichTextBox rc)
+        {
+            string[] FolderListing = Directory.GetDirectories(dizin);
+            for (int i = 0; i < FolderListing.Length; i++)
+            {
+                rc.AppendText(FolderListing[i].ToString() + "\r");
+                lbl.Text = "Delete Cookie :" + deleteid;deleteid++;
+                try
+                {
+                    Directory.Delete(FolderListing[i], true);
+                }
+                catch
+                {
+
+                    try
+                    {
+                        Directory.Delete(FolderListing[i], true);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.HResult.ToString()== "0x80070020")
+                        {
+                            rc.AppendText("Dosya Kullanılıyor" + "\r");
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ EBSSeleniumTempClear/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+namespace EBSSeleniumTempClear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        EBSSeleniumTempClear clear = new EBSSeleniumTempClear();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clear.seleniumCookiedelete(label1,richTextBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear.TempClear(label1,richTextBox1);
+        }
+    }
+}
